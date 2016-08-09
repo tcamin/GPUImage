79c2f8b8a7eee2af88f4844091ca204c83691f53
Tomas Camin <tomas.camin@scmitaly.it>
2016-08-09 21:57:04 +0200

    Properly lock and unlock renderTarget

--- framework/Source/GPUImageFramebuffer.m
+++ framework/Source/GPUImageFramebuffer.m
@@ -251,6 +251,8 @@ void dataProviderUnlockCallback (void *info, const void *data, size_t size);
 
 - (void)lock;
 {
+    CVPixelBufferLockBaseAddress(renderTarget, 0);
+    
     if (referenceCountingDisabled)
     {
         return;
@@ -261,6 +263,8 @@ void dataProviderUnlockCallback (void *info, const void *data, size_t size);
 
 - (void)unlock;
 {
+    CVPixelBufferUnlockBaseAddress(renderTarget, 0);
+    
     if (referenceCountingDisabled)
     {
         return;
